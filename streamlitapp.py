c4ed17223d18a6c2afe22347ccdbb7fe29a91ca3
adit-codes <aditirenuprakash@gmail.com>
2026-02-28 16:28:55 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -11,9 +11,9 @@ st.set_page_config(page_title="Telehealth AI Assistant", layout="wide")
 N8N_WEBHOOK_DOCTOR_CONFIG = "https://bytebeez.app.n8n.cloud/webhook/DOCinp1"
 N8N_WEBHOOK_WORKFLOW_X_MANUAL = "https://bytebeez.app.n8n.cloud/webhook/WORKFLOW_X_MANUAL"
 N8N_WEBHOOK_WORKFLOW_Y_AI = "https://bytebeez.app.n8n.cloud/webhook/DOCinpY"
-N8N_WEBHOOK_GET_PATIENT_PARAMS = "https://bytebeez.app.n8n.cloud/webhook-test/patientINP"
+N8N_WEBHOOK_GET_PATIENT_PARAMS = "https://bytebeez.app.n8n.cloud/webhook/patientINP"
 N8N_WEBHOOK_PROCESS_SUBMISSION = "https://your-n8n-instance.com/webhook/process-submission"
-N8N_WEBHOOK_WORKFLOW_Z = "https://bytebeez.app.n8n.cloud/webhook-test/getParameters"
+N8N_WEBHOOK_WORKFLOW_Z = "https://bytebeez.app.n8n.cloud/webhook/getParameters"
 
 # Helper function to generate a unique Patient ID
 def generate_patient_id(name):
